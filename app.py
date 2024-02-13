5148a4457b60dad9ae4555a76023c3776c85dfcc
Phoenixcresset <jeanbaptiste.lambertin@gmail.com>
2024-02-13 12:15:31 +0100

    Imports

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+import flask
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import login_required, logout_user, LoginManager, login_user
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -6,7 +10,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():  # put application's code here
     return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run()
