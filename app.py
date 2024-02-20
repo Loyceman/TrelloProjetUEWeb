38400ded37051c4bb1b1ee8e29f663b47b663d0f
Phoenixcresset <jeanbaptiste.lambertin@gmail.com>
2024-02-20 14:47:44 +0100

    finished database

--- app.py
+++ app.py
@@ -3,26 +3,37 @@ from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_login import login_required, logout_user, login_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from database.database import db, init_database
-from database.models import User
+from database.models import User, Task
+import os
+
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database/database.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///../database/database.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
 db.init_app(app)
 
+with app.test_request_context():
+    init_database()
 
-user_1 = User(username="Flo", password_hash="oui")
-db.session.add(user_1)
-db.commit()
-print(User.query.all)
 
 @app.route('/')
 def dashboard():
     # TODO
+    user_1 = User(username="Flo", password_hash="oui")
+    db.session.add(user_1)
+    task_carae = Task(label="Réserver salle CARAE", isDone=False)
+    db.session.add(task_carae)
+    db.session.commit()
+
+    users = User.query.all()
+    for user in users:
+        print(user.id, user.username, user.password_hash)
+    tasks = Task.query.all()
+    for task in tasks:
+        print(task.id, task.label, task.isDone)
     return 'Hello World!'
 
 
@@ -38,7 +49,7 @@ def login():
     if request.method == 'POST':
         # TODO
         return redirect('/')
-    else :
+    else:
         return render_template('login.html')
 
 
@@ -47,7 +58,7 @@ def register():
     if request.method == 'POST':
         # TODO
         return redirect('/')
-    else :
+    else:
         return render_template('register.html')
 
 
