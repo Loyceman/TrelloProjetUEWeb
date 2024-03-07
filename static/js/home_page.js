cd7a89502ebd0886e2e8b6d2152e5f4a787ff8ee
Théo Granier <127297775+theoo33@users.noreply.github.com>
2024-03-07 02:27:33 +0100

    apparence projets v2

--- static/js/home_page.js
+++ static/js/home_page.js
@@ -13,15 +13,26 @@ function onLoad() {
         closeOnSelect: false,
     });
 
-    const icon_unfold = document.getElementById("icon-unfold-projects")
-    icon_unfold.addEventListener("click", function() {
-        if (icon_unfold.src.includes("chevron-down")) {
-            icon_unfold.src = icon_unfold.src.replace("down","up")
+    let change_icon_unfold = function() {
+        if (document.getElementById("project-collapser").getAttribute("aria-expanded") === "true") {
+            icon_projects_unfold.src = icon_projects_unfold.src.replace("down","up")
         }
         else {
-            icon_unfold.src = icon_unfold.src.replace("up","down")
+            icon_projects_unfold.src = icon_projects_unfold.src.replace("up","down")
         }
-    })
+        if (document.getElementById("task-collapser").getAttribute("aria-expanded") === "true") {
+            icon_tasks_unfold.src = icon_tasks_unfold.src.replace("down","up")
+        }
+        else {
+            icon_tasks_unfold.src = icon_tasks_unfold.src.replace("up","down")
+        }
+    }
+
+    const icon_projects_unfold = document.getElementById("icon-unfold-projects")
+    icon_projects_unfold.addEventListener("click", change_icon_unfold)
+    const icon_tasks_unfold = document.getElementById("icon-unfold-tasks")
+    icon_tasks_unfold.addEventListener("click", change_icon_unfold)
+    change_icon_unfold()
 
     $("#button_create_project").click(function () {
         let name = $("#name_new_project").val()
@@ -84,22 +95,25 @@ function updateProjectList() {
                     const newLayout = document.createElement("div")
                         newLayout.className = "btn-project-layout p-0 col-xl-3 col-sm-6 col-12"
                     const newButton = document.createElement('button');
-                        newButton.className = "btn btn-project p-0";
+                        newButton.className = "btn btn-project";
                         newButton.id = "buttonProject";
                         newButton.value = project["id"];
-                        newButton.style.border = "2px solid"
                         newButton.style.borderColor = project["color"]
+                        newButton.style.backgroundColor = project["color"].concat("0A");
+                        newButton.style.boxShadow = "0 0 5px 0".concat(project["color"]).concat("33")
+                    const newColorBar = document.createElement("span")
+                        newColorBar.className = "btn-project-colorbar"
+                        newColorBar.style.backgroundColor = project["color"]
                     const newTitle = document.createElement("span")
                         newTitle.className = "btn-project-title"
                         newTitle.textContent = project["name"];
-                        newTitle.style.backgroundColor = project["color"];
                     const newDesc = document.createElement("span");
-                        newDesc.className = "btn-project-desc p-2";
-                        newDesc.style.backgroundColor = "var(--c-light)"
+                        newDesc.className = "btn-project-desc";
                         newDesc.textContent = project["description"];
 
 
                     newLayout.appendChild(newButton)
+                    newButton.appendChild(newColorBar)
                     newButton.appendChild(newTitle)
                     newButton.appendChild(newDesc)
                     document.getElementById("listProject").appendChild(newLayout);
